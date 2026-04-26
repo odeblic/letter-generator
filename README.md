9d43802e2d906b690d71c35f22964db7b7ab3a28
Olivier de Blic <odeblic@workstation.home.arpa>
2026-04-26 17:33:18 +0800

    Add source code and README

---
+++ README.md
@@ -0,0 +1,82 @@
+# Formal Letter Generator
+
+## Description
+
+Generate letters for different purposes under different identities.
+
+The purpose is determined by the `Context` and the `Template` of the document.
+
+The identity is provided by the `Sender` and its signature.
+
+Documents are generated as PDF files.
+
+## Configuration
+
+Sender:
+
+Contact details of the sender and signature if any.
+
+```yaml
+full_name: "John DOE"               # required
+phone_number: "(+1) 123456789"      # required
+email_address: "john.doe@gmail.com" # required
+signature_file: "signature.png"     # optional, replaced by fullname if not provided
+```
+
+Senders are defined as `.yaml` files and located in `./senders/`.
+
+Context:
+
+Definition of the context. `variables` are used for **Jinja2** substitutions in text.
+
+```yaml
+label: "Job application"     # optional
+template: "cover-letter"     # required
+sender: "joe"                # required
+variables:                   # optional
+  company: "OKAM Trade Ltd."
+  position: "Vendor"
+  recruiter: "Alicia WONG"
+date: "1999-12-31"           # optional, defaulted to today's date
+```
+
+Contexts are defined as `.yaml` files and located in `./contexts/`.
+
+Template:
+
+Content of the document itself. All fields are required.
+If mentioned, variables `%COMPANY%` and `%TITLE%` are substituted
+respectively for `attention` and `subject` with values from the role.
+
+```yaml
+attention: "Hiring Manager at {{ company }}"
+subject: "Application for {{ position }} position"
+greeting: "Dear {{ recruiter }},"
+content:
+  - "I would like to sell stuff on your behalf."
+  - "Please hire me."
+  - "You can count on my loyalty and dedication."
+closing: "Sincerely,"
+```
+
+Templates are defined as `.yaml` files and located in `./templates/`.
+
+Overall, the configuration layout looks like this:
+
+```
+.
+|-- contexts
+|   `-- application-joe-okam.yaml
+|-- senders
+|   |-- joe.yaml
+|   `-- signature.png
+`-- templates
+    `-- cover-letter.yaml
+```
+
+## Usage
+
+```sh
+python3 generator.py
+ls output
+```
---
+++ generate.py
@@ -0,0 +1,200 @@
+import datetime
+import glob
+import jinja2
+import os
+import shutil
+import subprocess
+import yaml
+from dataclasses import dataclass
+from pathlib import Path
+
+
+def render(template: str, variables: dict[str, str]) -> str:
+    return jinja2.Template(template, undefined=jinja2.StrictUndefined).render(variables)
+
+
+@dataclass
+class Sender:
+    full_name: str
+    phone_number: str
+    email_address: str
+    signature_file: str | None = None
+
+    @classmethod
+    def read_yaml(cls, file: Path) -> 'Sender':
+        with open(file, 'r') as f:
+            data = yaml.safe_load(f)
+        return cls(**data)
+
+
+@dataclass
+class Context:
+    label: str
+    template: str
+    sender: str
+    variables: list[str]
+    date: datetime.date | None = None
+
+    @classmethod
+    def read_yaml(cls, file: Path) -> 'Context':
+        with open(file, 'r') as f:
+            data = yaml.safe_load(f)
+        if 'label' not in data:
+            data['label'] = data['template']
+        if 'variables' not in data:
+            data['variables'] = []
+        if 'date' in data:
+            data['date'] = datetime.datetime.strptime(data['date'], "%Y-%m-%d").date()
+        return cls(**data)
+
+
+@dataclass
+class Template:
+    attention: str
+    subject: str
+    greeting: str
+    content: list[str]
+    closing: str
+
+    @classmethod
+    def read_yaml(cls, file: Path) -> 'Template':
+        with open(file, 'r') as f:
+            data = yaml.safe_load(f)
+        return cls(**data)
+
+    def __write_contact(self, full_name: str, phone_number: str, email_address: str) -> None:
+        with open('contact.tex', 'w') as f:
+            f.write(f'{full_name}\n')
+            f.write('\\newline\n')
+            f.write(f'{phone_number}\n')
+            f.write('\\newline\n')
+            f.write(f'\\uline{{{email_address}}}\n')
+            f.write('\\newline\n')
+
+    def __write_date(self, date: datetime.date | None = None) -> None:
+        with open('date.tex', 'w') as f:
+            if date is None:
+                date = datetime.datetime.now()
+            text = date.strftime("%d. %B %Y")
+            f.write(f'\hfill {text}\n')
+            f.write('\\newline\n')
+
+    def __write_attention(self, variables: dict[str, str]) -> None:
+        with open('attention.tex', 'w') as f:
+            text = render(self.attention, variables)
+            f.write(f'\\uline{{\\textbf{{Attention:}}}}\n')
+            f.write(f'{text}\n')
+            f.write('\\newline\n')
+
+    def __write_subject(self, variables: dict[str, str]) -> None:
+        with open('subject.tex', 'w') as f:
+            text = render(self.subject, variables)
+            f.write(f'\\uline{{\\textbf{{Subject:}}}}\n')
+            f.write(f'{text}\n')
+            f.write('\\newline\n')
+
+    def __write_greeting(self, variables: dict[str, str]) -> None:
+        with open('greeting.tex', 'w') as f:
+            greeting = variables.get('greeting', self.greeting)
+            for line in greeting.split('\n'):
+                text = render(line, variables)
+                f.write(f'{text}\n')
+                f.write('\\newline\n')
+
+    def __write_content(self, variables: dict[str, str]) -> None:
+        with open('content.tex', 'w') as f:
+            for line in self.content:
+                text = render(line, variables)
+                f.write(f'{text}\n')
+                f.write('\\newline\n\n')
+
+    def __write_closing(self, variables: dict[str, str]) -> None:
+        with open('closing.tex', 'w') as f:
+            text = render(self.closing, variables)
+            f.write(f'{text}\n')
+            f.write('\\newline\n')
+
+    def __write_signature(self, signature_file: str | None, full_name: str) -> None:
+        with open('signature.tex', 'w') as f:
+            if signature_file is not None:
+                f.write('\\hspace{8cm}\n')
+                f.write(f'\\includegraphics[scale=0.2]{{senders/{signature_file}}}\n')
+            else:
+                f.write('\\hspace{8cm}\n')
+                f.write(f'{full_name}\n')
+
+    def write_latex(self, context: Context, sender: Sender) -> None:
+        self.__write_contact(sender.full_name, sender.phone_number, sender.email_address)
+        self.__write_date(context.date)
+        self.__write_attention(context.variables)
+        self.__write_subject(context.variables)
+        self.__write_greeting(context.variables)
+        self.__write_content(context.variables)
+        self.__write_closing(context.variables)
+        self.__write_signature(sender.signature_file, sender.full_name)
+
+
+def build_pdf(label: str) -> None:
+    folder = Path('output')
+    folder.mkdir(exist_ok=True)
+    try:
+        with open(f'output/pdflatex-{label}.log', 'w') as log_file:
+            command = ["pdflatex", f'-output-directory={folder.name}', f'-jobname={label}', "document.tex"]
+            subprocess.run(command, check=True, stdout=log_file, stderr=subprocess.STDOUT)
+        print('\033[32mCover letter generated with success\033[0m')
+    except subprocess.CalledProcessError as e:
+        print('\033[31mCover letter failed to be generated\033[0m')
+        print(f'\033[31mError: {e}.\033[0m')
+
+
+def cleanup(label: str) -> None:
+    folder = Path('output')
+    os.remove(folder / f'pdflatex-{label}.log')
+    os.remove(folder / f'{label}.log')
+    os.remove(folder / f'{label}.aux')
+    os.remove(folder / f'{label}.out')
+    os.remove('date.tex')
+    os.remove('greeting.tex')
+    os.remove('attention.tex')
+    os.remove('subject.tex')
+    os.remove('closing.tex')
+    os.remove('contact.tex')
+    os.remove('content.tex')
+    os.remove('signature.tex')
+
+
+# unused
+def move_documents() -> None:
+    folder = Path('documents')
+    folder.mkdir(exist_ok=True)
+    for pdf_file in glob.glob('output/*.pdf'):
+        source_file = Path(pdf_file)
+        destination_file = folder / source_file.name
+        print(f'\033[35mGetting document {destination_file.name}\033[0m')
+        shutil.move(source_file, destination_file)
+
+
+def main() -> None:
+    for context_file in glob.glob('contexts/*.yaml'):
+        print(f'\033[34mLooking at file {context_file}\033[0m')
+        context = Context.read_yaml(context_file)
+        print(f'\033[34mProcessing context {context}.\033[0m')
+        template_file = f'templates/{context.template}.yaml'
+        print(f'\033[34mLooking at file {template_file}\033[0m')
+        template = Template.read_yaml(template_file)
+        print(f'\033[34mUsing template {template}.\033[0m')
+        sender_file = f'senders/{context.sender}.yaml'
+        print(f'\033[34mLooking at file {sender_file}\033[0m')
+        sender = Sender.read_yaml(sender_file)
+        print(f'\033[34mUsing sender {sender}.\033[0m')
+        try:
+            template.write_latex(context, sender)
+        except jinja2.exceptions.UndefinedError as e:
+            print(f'\033[31mError at variable substitution: {e}.\033[0m')
+            continue
+        build_pdf(context.label)
+        cleanup(context.label)
+
+
+if __name__ == '__main__':
+    main()
